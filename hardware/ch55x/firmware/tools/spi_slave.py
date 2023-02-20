d678db624851a3feafbc4522ca2ac2906e8737ba
YuLong Yao <feilongphone@gmail.com>
2023-02-20 20:16:35 +0800

    pg

--- hardware/ch55x/firmware/tools/spi_slave.py
+++ hardware/ch55x/firmware/tools/spi_slave.py
@@ -6,6 +6,7 @@ import logging
 from usb2x import CH55xDevice
 
 log: logging.Logger = logging.getLogger(name='usb2x')
+log.setLevel(logging.NOTSET)
 
 
 class USB2XRegisters(dict):
@@ -24,52 +25,76 @@ class USB2XRegisters(dict):
 
 class USB2XPin():
     # pin = port.pin, example: port2 - pin3 = 2.3
-    def __init__(self, reg: dict, pin: float) -> None:
-        self.pin = pin
+    def __init__(self, reg: dict, pinid: float) -> None:
+        self.pinid = pinid
         self.reg = reg
+
+        self.port, self.pin = str(self.pinid).split('.')
+        self.pin = int(self.pin)
         pass
 
     def __eq__(self, other):
         match other:
             case int() | float():
-                return self.pin == other
+                return self.pinid == other
 
             case USB2XPin():
-                return self.pin == other.pin
+                return self.pinid == other.pinid
 
             case _:
                 log.critical(f'can not eq with {type(other)}')
 
     def __le__(self, other):
+        val = self.reg['sfr'][f'P{self.port}'][0]
+        bit = (1 << self.pin)
         match other:
             case 0:
-                self.reg['sfr']['P1'] = 0xef
+                self.reg['sfr'][f'P{self.port}'] = val & ~bit
             case 1:
-                self.reg['sfr']['P1'] = 0xff
-            # case 'z':
+                self.reg['sfr'][f'P{self.port}'] = val | bit
+            # open drain
+            case 'z':
+                self.reg['sfr'][f'P{self.port}'] = val | bit
+                self.reg['sfr'][f'P{self.port}_MOD_OC'] = val | bit
+            # no pull-up
+            case 'x':
+                self.reg['sfr'][f'P{self.port}_DIR_PU'] = val & ~bit
+            # pull up
+            case 'u':
+                self.reg['sfr'][f'P{self.port}_DIR_PU'] = val | bit
+            # push-pull
+            case 'p':
+                self.reg['sfr'][f'P{self.port}_MOD_OC'] = val & ~ bit
             case _:
                 log.critical(f'can not set value {type(other)} to pin {self}')
 
+    def __int__(self):
+        return self.reg['sfr'][f'P{self.port}']
+
     def __repr__(self) -> str:
-        return str(self.pin)
+        return str(self.pinid)
 
 
 class USB2XPinList(dict):
     def __init__(self, reg: dict, pinList: list):
-        self.pinlist = pinList
         self.reg = reg
+        self.pinmap = {}
 
-    def __getitem__(self, key: float):
-        return USB2XPin(self.reg, key)
+        for p in pinList:
+            self.pinmap[p] = USB2XPin(self.reg, p)
 
-    def items(self):
-        return {USB2XPin(self.reg, p) for p in self.pinlist}
+    def __getitem__(self, key):
+        return self.pinmap[key]
+
+    def __contains__(self, item):
+        return item in self.pinmap.keys()
 
     def __repr__(self) -> str:
-        return ','.join({str(p) for p in self.pinlist})
+        # return ','.join({str(p) for p in self.pinlist})
+        return ','.join({str(v) for p, v in self.pinmap.items()})
 
 
-class CH552:
+class CH55x:
     def __init__(self):
         self.dev = CH55xDevice()
         self.regs = {
@@ -80,7 +105,33 @@ class CH552:
             }),
         }
 
-        # if ch552e
+    def port(self, method: str, pin):
+        if pin not in self.pins:
+            log.critical(f'Port "{ pin }" Not Found, We only have {self.pins}')
+
+        match method:
+            case 'sethigh' | 'high' | 'h':
+                self.pins[pin] <= 1
+            case 'setlow' | 'low' | 'l':
+                self.pins[pin] <= 0
+            case 'setopendrain' | 'setod' | 'od' | 'z':
+                self.pins[pin] <= 'z'
+            case 'pushpull':
+                self.pins[pin] <= 'p'
+            case 'nopullup' | 'npu' | 'x':
+                self.pins[pin] <= 'x'
+            case 'pullup' | 'pu' | 'u':
+                self.pins[pin] <= 'u'
+            case 'get' | 'read':
+                return int(self.pins[pin])
+            case _:
+                log.critical(f'Method "{method}" Not Support')
+
+
+class CH552E(CH55x):
+    def __init__(self):
+        super().__init__()
+
         self.pins = USB2XPinList(self.regs, [
             # port.pin
             1.4,
@@ -91,22 +142,6 @@ class CH552:
             # usb port can't use
         ])
 
-    def port(self, method: str, pin):
-        if pin not in self.pins:
-            log.critical(f'Port "{ pin }" Not Found, We only have {self.pins}')
-
-        match method:
-            case 'sethigh' | 'high' | 'h':
-                self.pins[pin] <= 1
-            case 'setlow' | 'low' | 'l':
-                self.pins[pin] <= 0
-            case 'setod' | 'setopendrain' | 'od':
-                self.pins[pin] <= 'z'
-            case 'get' | 'read':
-                return self.pins[pin]
-            case _:
-                log.critical(f'Method "{method}" Not Support')
-
 
 if __name__ == "__main__":
-    fire.Fire(CH552)
+    fire.Fire(CH552E)
